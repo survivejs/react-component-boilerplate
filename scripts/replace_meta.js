8bbfbac3718041ec8fa5b46bc53d5866d6433c57
Juho Vepsalainen <bebraw@gmail.com>
2015-04-16 19:43:16 +0300

    Allow package author to be replaced

--- scripts/replace_meta.js
+++ scripts/replace_meta.js
@@ -9,11 +9,17 @@ main();
 
 function main() {
     var originals = {
+        author: meta.author,
         user: meta.user,
         project: meta.name,
     };
     var schema = {
         properties: {
+            author: {
+                description: 'Enter package author',
+                default: originals.author,
+                required: true
+            },
             user: {
                 description: 'Enter GitHub user/organization name',
                 default: originals.user,
@@ -38,6 +44,22 @@ function main() {
             'README.md',
             'package.json',
         ];
+
+        if(originals.author !== result.author) {
+            console.log('replacing author');
+
+            replace({
+                regex: originals.author,
+                replacement: result.author,
+                paths: [
+                    'LICENSE',
+                    'package.json',
+                ],
+                recursive: true,
+                silent: false,
+                count: true,
+            });
+        }
 
         if(originals.user !== result.user) {
             console.log('replacing users/organizations');
