077fb8fbb00eaad7a2046e3fb6749809dd90c795
Juho Vepsalainen <bebraw@gmail.com>
2015-05-02 11:52:33 +0300

    Push dev server ip to higher level

--- dev-server/server.js
+++ dev-server/server.js
@@ -3,15 +3,16 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 
 var config = require('../config/');
+var ip = '0.0.0.0';
 
 new WebpackDevServer(webpack(config), {
     contentBase: __dirname,
     publicPath: config.output.publicPath,
     hot: true
-}).listen(config.port, '0.0.0.0', function(err) {
+}).listen(config.port, ip, function(err) {
     if (err) {
         return console.log(err);
     }
 
-    console.log('Listening at 0.0.0.0:' + config.port);
+    console.log('Listening at ' + ip + ':' + config.port);
 });
