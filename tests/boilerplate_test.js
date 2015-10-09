40e19e1ad0305c003737e24d378cee5c12cefdc5
Juho Vepsalainen <bebraw@gmail.com>
2015-10-09 14:18:27 +0300

    Set up Karma/Mocha/Chai/Phantom based tests
    Closes #13.
    Closes #7.
    Closes #6.

---
+++ tests/boilerplate_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// import React from 'react';
+// import {
+//   renderIntoDocument,
+//   findRenderedDOMComponentWithClass,
+//   findRenderedDOMComponentWithTag,
+//   Simulate
+//} from 'react-addons-test-utils';
+
+describe('Boilerplate', function() {
+  it('should do boilerplate things', function() {
+    // TODO: test something now
+    expect(true).to.equal(true);
+  });
+});
