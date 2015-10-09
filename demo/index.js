8a36c434d2310269c0d18a880c68fa4784583010
Juho Vepsalainen <bebraw@gmail.com>
2015-10-09 13:22:25 +0300

    Set up index to work with isomorphism
    Now React renders to the correct element.

--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,19 @@
-import 'purecss';
-import 'highlight.js/styles/github.css';
-import 'react-ghfork/gh-fork-ribbon.css';
-import './main.css';
-import '../style.css';
+'use strict';
 
-import React from 'react';
-import App from './app.jsx';
+require('purecss');
+require('highlight.js/styles/github.css');
+require('react-ghfork/gh-fork-ribbon.ie.css');
+require('react-ghfork/gh-fork-ribbon.css');
+require('./main.css');
+require('../style.css');
+
+var React = require('react');
+var App = require('./app.jsx');
 
 main();
 
 function main() {
-  var app = document.createElement('div');
-  document.body.appendChild(app);
+  var app = document.getElementsByClassName('demonstration')[0];
 
   React.render(<App />, app);
 }
